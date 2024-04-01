1dbd4ab47960f4af7a58f1fa35d8a4b444e02c36
@hexlet/cli <support@hexlet.io>
2024-04-01 20:28:34 +0300

    submit java-spring-ru/dependency-injection

---
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -0,0 +1,61 @@
+package exercise.controller;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import exercise.exception.ResourceNotFoundException;
+
+// BEGIN
+
+@RestController
+@RequestMapping("/comments")
+public class CommentsController {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping
+    public List<Comment> getAllPosts() {
+        return commentRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Comment getById(@PathVariable long id) {
+        return commentRepository.findById(id).get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment create(@RequestBody Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    @PutMapping(path = "/{id}")
+    public Comment update(@PathVariable long id, @RequestBody Comment comment)
+    {
+        return commentRepository.save(comment);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable Long id) {
+        commentRepository.deleteById(id);
+    }
+
+}
+// END
